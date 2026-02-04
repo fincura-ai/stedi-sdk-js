998e95e1324ced33a71b1bca9f543ded933084de
Louis-P. Lamoureux <93926113+louis-lamoureux@users.noreply.github.com>
2026-02-04 16:21:33 -0500

    feat(providers): add endpoints (#13)
    - add get endpoint
    - add list endpoint

--- src/endpoints/provider.ts
+++ src/endpoints/provider.ts
@@ -1,6 +1,7 @@
 import { type StediClient } from '../lib/client.js';
 import {
   type StediProviderInput,
+  type StediProviderListResponse,
   type StediProviderResponse,
 } from '../lib/types.js';
 
@@ -20,6 +21,34 @@ export const provider = (client: StediClient, baseUrl: string) => {
       return await client.request(baseUrl, 'POST', '/providers', {
         data: input,
       });
+    },
+
+    /**
+     * Get a Stedi provider by ID
+     *
+     * @see https://www.stedi.com/docs/api-reference/healthcare/get-enrollment-provider
+     *
+     * @param providerId - The ID of the provider to retrieve.
+     * @returns A promise that resolves to the provider response.
+     */
+    get: async (providerId: string): Promise<StediProviderResponse> => {
+      return await client.request(
+        baseUrl,
+        'GET',
+        `/providers/${providerId}`,
+        {},
+      );
+    },
+
+    /**
+     * List all Stedi providers
+     *
+     * @see https://www.stedi.com/docs/api-reference/healthcare/get-enrollment-providers
+     *
+     * @returns A promise that resolves to the list of providers.
+     */
+    list: async (): Promise<StediProviderListResponse> => {
+      return await client.request(baseUrl, 'GET', '/providers', {});
     },
   };
 };
